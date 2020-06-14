25b36904803ae3f20afcbf1e85b20332faea62f0
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-14 21:05:34 +0900

    added on the frontend files and make them a filewhitelist to track if the path is legal

--- mod.ts
+++ mod.ts
@@ -1,10 +1,47 @@
-import { Application } from "https://deno.land/x/oak@v5.0.0/mod.ts";
+import { Application, send } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 
 const app = new Application();
 const PORT = 8000;
 
-app.use((ctx) => {
-  ctx.response.body = `Nasa Mission Control API`;
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
+  console.log(delta);
+});
+
+app.use(async (ctx) => {
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/styleshets/style.css",
+    "/images/favicon.png",
+  ];
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}/public`,
+    });
+  }
+});
+
+app.use(async (ctx) => {
+  ctx.response.body = `
+  {___     {__      {_        {__ __        {_
+  {_ {__   {__     {_ __    {__    {__     {_ __
+  {__ {__  {__    {_  {__    {__          {_  {__
+  {__  {__ {__   {__   {__    {__        {__   {__
+  {__   {_ {__  {______ {__      {__    {______ {__
+  {__    {_ __ {__       {__ {__  {__  {__       {__
+  {__      {__{__         {__  {__ __ {__         {__
+                  Mission Control API`;
 });
 
 if (import.meta.main) {
