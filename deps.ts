3a7ea8f8585b71c2f799cf79ecd54ee25baa4836
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-20 18:59:56 +0900

    setup the lock.json

--- deps.ts
+++ deps.ts
@@ -4,9 +4,6 @@ export { join } from "http://deno.land/std@0.58.0/path/mod.ts";
 export { BufReader } from "http://deno.land/std@0.58.0/io/bufio.ts";
 export { parse } from "http://deno.land/std@0.58.0/encoding/csv.ts";
 
-
-
-
 // Third party dependencies
 export { Application, send, Router } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 export { pick, flatMap } from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
